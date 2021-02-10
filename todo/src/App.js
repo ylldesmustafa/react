bbddc3a0b0141f001141167939f8caaabbafe7a3
ylldesmustafa <77593205+ylldesmustafa@users.noreply.github.com>
2021-02-09 19:05:51 -0600

    added app.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,19 +1,19 @@
-// // import logo from './logo.svg';
-// import { useState } from 'react';
+// import logo from './logo.svg';
+import { useState } from 'react';
 
-// function App() {
+function App() {
 
-//   // const [todos, setTodos] = useState(["eat", "sleep", "code", "pray", "repeat"]);  //sample todo items.  these will need to be changed in your app.  Just filler todos, although they are quite important!
+  // const [todos, setTodos] = useState(["eat", "sleep", "code", "pray", "repeat"]);  //sample todo items.  these will need to be changed in your app.  Just filler todos, although they are quite important!
 
-//   const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//        <p>To Do List</p>
-//       </header>
-//     </div>
-//   );
-// }
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>To Do List</p>
+      </header>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
